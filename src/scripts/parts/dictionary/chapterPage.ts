d52ff335cf1dff2fa2aa310d3f46e8c3920543ae
SashaTsarikova <Sashatsarikova@mail.ru>
2022-02-05 22:50:37 +0300

    feat: partly create layout for dictionary

---
+++ src/scripts/parts/dictionary/chapterPage.ts
@@ -0,0 +1,20 @@
+const chpterLayout = ` <main class="main bg-dark">
+<div class="container py-3">
+    <h2 class="text-center text-white pb-2">... Выбери уровень сложности ...</h2>
+    <div class="row row-cols-1 row-cols-md-3 g-4">    
+    </div>
+</div>
+</main>`;
+
+const chapterItem = `<div class="col">
+<div class="card">
+    <div class="card-body colored-purple">
+    <h3 class="card-title text-center" data-number="1">1</h3>
+    <p class="card-text special-text">Сааамый легкий. Если не пельмешка, все будет чики</p>
+    </div>
+    <div class="card-footer">
+        <small class="text-muted">Изучи эту категорию</small>
+    </div>
+</div>
+</div>`;
+
