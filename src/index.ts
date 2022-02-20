cdf624e881aaec76964db31d4b8805d8fa2d368c
SashaTsarikova <Sashatsarikova@mail.ru>
2022-02-20 13:41:49 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import './scss/main.scss';
 import 'bootstrap';
 import renderBasicLayout from './scripts/bacis-layout';
-import {getStatistics, setStatistics} from './scripts/api/statisticsF';
 
 renderBasicLayout();
-
-// getStatistics();
-
-// setStatistics('audio-call', 123 ,['1998','1', '9'], ['2', '4'], 3);
-getStatistics().then((el) => console.log(el));
