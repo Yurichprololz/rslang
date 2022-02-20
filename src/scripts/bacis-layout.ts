b800b9085cef7ad29baf5070e5af5b5247a8c28c
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-02-20 12:50:26 +0300

    fix: main conponent

--- src/scripts/bacis-layout.ts
+++ src/scripts/bacis-layout.ts
@@ -5,6 +5,7 @@ import renderChaptersPage from './parts/dictionary/chapterPage';
 import renderWordsPage from './parts/dictionary/wordlistPage';
 import renderChaptersMiniPage from './parts/mini-games/chapterMiniPage';
 import updateHeader from './auth';
+import mainHomeLayout from './parts/home/mainLayout';
 
 function createHeader():HTMLElement {
   const header = createElement('header', 'header p-3 bg-dark text-white') as HTMLElement;
@@ -30,31 +31,7 @@ function createFooter():HTMLElement {
 
 function createMain():HTMLElement {
   const main = createElement('main', 'main bg-dark text-white') as HTMLElement;
-  main.innerHTML = `
-  <div class="container">
-    <h1 class="h1 text-white text-center">RSLang</h1>
-    <p class="text-white text-center">Используй быстрый и эффективный способ изучения английского языка.
-        Все самые лучшие методики в одном месте.</p>
-      <h2 class='text-center'>О нас</h2>
-      <div class="container text-black d-flex flex-wrap justify-content-around">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Юрий Таргонский</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="https://github.com/Yurichprololz" target="_blank" class="btn btn-primary">Гитхаб</a>
-            </div>
-          </div>
-        <div class="card" style="width: 18rem;">
-          <img src="../assets/images/sasha.jpg" class="card-img-top" alt="">
-          <div class="card-body">
-            <h5 class="card-title">Александра Царикова</h5>
-            <p class="card-text">Frontend developer </br> Настроила взаимодействие с бэкэндом, создала страницы разделов и слов, разработала игру "Спринт"</p>
-            <a href="https://github.com/sashatsarikova" target="_blank" class="btn btn-primary">Гитхаб</a>
-          </div>
-        </div>
-  </div>
-        </div>`;
+  main.innerHTML = mainHomeLayout;
   return main;
 }
 
