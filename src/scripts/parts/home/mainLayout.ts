2a3119cf1b248eac78a48a0bf844c6abdee67c9a
Yurichprololz <84023042+Yurichprololz@users.noreply.github.com>
2022-02-20 22:20:09 +0300

    feat: info

--- src/scripts/parts/home/mainLayout.ts
+++ src/scripts/parts/home/mainLayout.ts
@@ -5,10 +5,14 @@ const mainHomeLayout = `<div class="container">
   <h2 class='text-center'>О нас</h2>
   <div class="container text-black d-flex flex-wrap justify-content-around">
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="./assets/images/photo_yuri.jpg" style="
+        height: 343px;
+        object-fit: cover;
+    " class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Юрий Таргонский</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p class="card-text">Тимлид, разработал архитектуру приложения и руководил командой. Разработал дизайн приложения, настроил аутентификацию, разработал мини-игру "Аудиовызов".
+          </p>
           <a href="https://github.com/Yurichprololz" target="_blank" class="btn btn-primary">Гитхаб</a>
         </div>
       </div>
