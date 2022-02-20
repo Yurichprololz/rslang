5fe9d8c99e7934e8abecc1bdc29409349f598c54
SashaTsarikova <Sashatsarikova@mail.ru>
2022-02-20 11:39:29 +0300

    feat: create api for stat

---
+++ src/scripts/api/statisticsF.ts
@@ -0,0 +1,86 @@
+import DOMAIN from './domain';
+import { General, SubGeneral } from '../interfaces/generalEnum';
+import { IStat, IForOptional } from '../interfaces/statisticsInterface';
+import { StorageItems } from '../interfaces/usersInterface';
+
+function shapeFetchStr(): string {
+  return `${DOMAIN}${General.users}/${localStorage.getItem(StorageItems.id)}${SubGeneral.statistics}`;
+}
+
+async function getStatistics() {
+  const rawResponse = await fetch(`${shapeFetchStr()}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem(StorageItems.token)}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  if (rawResponse.ok) {
+    const content: IStat = await rawResponse.json();
+    return content.optional;
+  }
+  if (rawResponse.status === 404) {
+    return rawResponse.status;
+  }
+  return rawResponse.status;
+}
+
+async function setStatistics(
+  gameName:'sprint' | 'audio-call',
+  idDate:number,
+  rightArr: string[],
+  wrongArr: string[],
+  sessionLength: number,
+) {
+  getStatistics().then(async (el) => {
+    const objectToSet = gameName === 'sprint'
+      ? {
+        sprint: {
+          [idDate]: [rightArr, wrongArr, sessionLength],
+        },
+        'audio-call': {},
+      } : {
+        sprint: {},
+        'audio-call': {
+          [idDate]: [rightArr, wrongArr, sessionLength],
+        },
+      };
+
+    if (el === 404) {
+      const rawResponse = await fetch(`${shapeFetchStr()}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(StorageItems.token)}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          learnedWords: 0,
+          optional: objectToSet,
+        }),
+      });
+      const content: IStat = await rawResponse.json();
+      return content.optional;
+    }
+    const rawResponse = await fetch(`${shapeFetchStr()}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem(StorageItems.token)}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        learnedWords: 0,
+        optional: {
+          sprint: { ...(<IForOptional>el)?.sprint, ...objectToSet.sprint },
+          'audio-call': { ...(<IForOptional>el)?.['audio-call'], ...objectToSet['audio-call'] },
+        },
+      }),
+    });
+    const content: IStat = await rawResponse.json();
+    return content.optional;
+  });
+}
+
+export { getStatistics, setStatistics };
